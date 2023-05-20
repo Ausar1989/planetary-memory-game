ab9f16303d56b5ea0590c3d204a18872dacf57d8
Jermaine Lewis <lewisjermaine22@gmail.com>
2023-05-20 10:56:35 -0500

    Updates

---
+++ precache-manifest.57343a4311b0d8fa6a4532909e9c3577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "381780de10de9fc9d35b",
+    "url": "/planetary-memory-game/static/css/main.56ea4536.chunk.css"
+  },
+  {
+    "revision": "381780de10de9fc9d35b",
+    "url": "/planetary-memory-game/static/js/main.381780de.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/planetary-memory-game/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "12138fb37e1f49b2e206",
+    "url": "/planetary-memory-game/static/js/2.12138fb3.chunk.js"
+  },
+  {
+    "revision": "5126a2686228309b404c",
+    "url": "/planetary-memory-game/static/js/runtime~main.5126a268.js"
+  },
+  {
+    "revision": "4b60a25ef5b5750b4afb7a32df068d17",
+    "url": "/planetary-memory-game/static/media/SpaceStation.4b60a25e.jpg"
+  },
+  {
+    "revision": "a3c026c5ea875165ec8f32535fac5d90",
+    "url": "/planetary-memory-game/index.html"
+  }
+];
